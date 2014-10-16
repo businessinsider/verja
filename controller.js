f6243559416100aaa8f4b45e3047c5adbbc4a888
Forrest Almasi <forrest.almasi@gmail.com>
2014-10-16 17:03:28 -0400

    data structure set up

--- controller.js
+++ controller.js
@@ -1,37 +1,41 @@
 var app = angular.module('app', []);
 
-
-
-
-
-app.controller('formController', function($scope, Validator) {
-		$scope.items = ["hello world","something","something else"];
+app
+	.controller('formController', function($scope, Validator) {
 
 		$scope.post = {
 			name: 'some kinda string',
 			email: 'validemail@email.com',
-			comments: [
-				{date: Date.now(), message: 'a cool message'}, 
-				{date: 1413486500150, message: 'this message was made'}
-			],
+			comments: [{
+				date: Date.now(),
+				message: 'a cool message'
+			}, {
+				date: 1413486500150,
+				message: 'this message was made'
+			}],
 			title: 'this-title-doesn\'t exist'
 		};
 
-
 		$scope.validPost = Validator.validate($scope.post);
 
+		$scope.addComment = function() {
+			$scope.post.comments.push({date: Date.now(), message: ''});
+		};
+
+		$scope.deleteComment = function(index) {
+			$scope.post.comments.splice(index,1);
+		}
+
+	})
+	.service('Validator', function() {
+
+		function validate(obj) {
+			obj.validated = true;
+			return obj;
+		}
+
+		return {
+			validate: validate
+		}
 
 	});
-
-app.service('Validator', function(){
-
-	function validate(obj) {
-		obj.validated = true;
-		return obj;
-	}
-
-	return {
-		validate: validate
-	}
-
-});
