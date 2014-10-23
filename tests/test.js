c1381267420627366f37e5b41e1cbc67eaf32e79
Forrest Almasi <forrest.almasi@gmail.com>
2014-10-23 14:27:23 -0400

    change more names

--- tests/test.js
+++ tests/test.js
@@ -1,24 +1,24 @@
 'use strict';
 
 var assert = require('assert');
-var validator = require('../verja.js');
+var verja = require('../verja.js');
 
-describe('validator', function() {
+describe('verja', function() {
 	
 	describe('addValidator', function() {
 
-		validator.addValidator('haskey', function(val) {
+		verja.addValidator('haskey', function(val) {
 			if (!val) return true;
 			return false;
 		});
 
 		var schema = {
-			key: new validator.Field({haskey: true})
+			key: new verja.Field({haskey: true})
 		};
 		var obj = {};
 		var errObj;
 
-		validator.validate(obj, schema, function(err) {
+		verja.validate(obj, schema, function(err) {
 			errObj = err;
 		});
 
@@ -31,7 +31,7 @@ describe('validator', function() {
 	describe('Field', function() {
 
 		var obj = {key: 'value'};
-		var field = new validator.Field(obj);
+		var field = new verja.Field(obj);
 
 		it('the Field constructor should return an object with the same properties', function() {
 			assert.equal(obj.key, field.key);
@@ -42,7 +42,7 @@ describe('validator', function() {
 		});
 
 		it('should be able to check if a field is an instance of Field', function() {
-			assert.equal(field instanceof validator.Field, true);
+			assert.equal(field instanceof verja.Field, true);
 		});
 
 	});
@@ -66,10 +66,10 @@ describe('validator', function() {
 
 		var nestSchema = {
 			key: {
-				str: new validator.Field({type: 'string'}),
-				obj: new validator.Field({type: 'object'})
+				str: new verja.Field({type: 'string'}),
+				obj: new verja.Field({type: 'object'})
 			},
-			key2: new validator.Field({required: true, type: 'boolean'})
+			key2: new verja.Field({required: true, type: 'boolean'})
 		};
 
 		it('should validate an array', function() {
@@ -78,9 +78,9 @@ describe('validator', function() {
 				key: ['string', {}]
 			};
 			var arrSch = {
-				key: [new validator.Field({type: 'string'})]
+				key: [new verja.Field({type: 'string'})]
 			};
-			validator.validate(objArr,arrSch, function(e) {
+			verja.validate(objArr,arrSch, function(e) {
 				errObj = e;
 			});
 
@@ -89,7 +89,7 @@ describe('validator', function() {
 
 		//need to fix the lib so this works
 		it('should validate a primative', function(done) {
-			validator.validate('some string', new validator.Field({type: 'string'}), function(e){
+			verja.validate('some string', new verja.Field({type: 'string'}), function(e){
 				if (e) { throw e; }
 				done();
 			});
@@ -97,7 +97,7 @@ describe('validator', function() {
 
 		//need to fix the lib so this works
 		it('should validate simple nested objects and return null for errors in the callback', function(done) {
-			validator.validate(goodNest, nestSchema, function(e) {
+			verja.validate(goodNest, nestSchema, function(e) {
 				if (e !== null) { throw e; }
 				done();
 			});
@@ -105,7 +105,7 @@ describe('validator', function() {
 
 		it('should validate simple nested objects (invalid example)', function() {
 			var errObj;
-			validator.validate(badNest, nestSchema, function(e) {
+			verja.validate(badNest, nestSchema, function(e) {
 				errObj = e;
 			});
 
@@ -118,7 +118,7 @@ describe('validator', function() {
 	//need to fix the lib to return null for no errors so this works
 	describe('validation rule: type', function() {
 		var schema = {
-			property: new validator.Field({type: 'string'})
+			property: new verja.Field({type: 'string'})
 		};
 
 		var obj = {
@@ -126,7 +126,7 @@ describe('validator', function() {
 		};
 
 		it ('should not throw an error when a string is passed in', function(done) {
-			validator.validate(obj, schema, function(err) {
+			verja.validate(obj, schema, function(err) {
 				if (err) { throw err; }
 				done();
 			});
@@ -134,7 +134,7 @@ describe('validator', function() {
 
 		it ('should throw an error when a string is not passed in', function(done) {
 			obj.property = [];
-			validator.validate(obj, schema, function(err) {
+			verja.validate(obj, schema, function(err) {
 				if (err) { return done(); }
 				throw err;
 			});
