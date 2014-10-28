420b751c54b84026031becca3edf648d3c920531
Forrest Almasi <forrest.almasi@gmail.com>
2014-10-28 15:27:32 -0400

    remove extra test file

--- tests/verja-api.js
+++ tests/verja-api.js
@@ -3,11 +3,11 @@
 var assert = require('assert');
 var verja = require('../verja.js');
 
-describe('verja API', function() {
+describe ('verja API', function() {
 
-	describe('addValidator', function() {
+	describe ('addValidator', function() {
 
-		it('addValidator adds a validator', function() {
+		it ('addValidator adds a validator', function() {
 			verja.addValidator('haskey', function(val) {
 				if (!val) return true;
 				return false;
@@ -48,26 +48,26 @@ describe('verja API', function() {
 
 	});
 
-	describe('Field', function() {
+	describe ('Field', function() {
 
 		var obj = {key: 'value'};
 		var field = new verja.Field(obj);
 
-		it('the Field constructor should return an object with the same properties', function() {
+		it ('the Field constructor should return an object with the same properties', function() {
 			assert.equal(obj.key, field.key);
 		});
 
-		it('the Field constructor should not return the same object', function() {
+		it ('the Field constructor should not return the same object', function() {
 			assert.notEqual(obj, field);
 		});
 
-		it('should be able to check if a field is an instance of Field', function() {
+		it ('should be able to check if a field is an instance of Field', function() {
 			assert.equal(field instanceof verja.Field, true);
 		});
 
 	});
 
-	describe('validate', function() {
+	describe ('validate', function() {
 
 		var goodNest = {
 			key: {
@@ -92,7 +92,7 @@ describe('verja API', function() {
 			key2: new verja.Field({required: true, type: 'boolean'})
 		};
 
-		it('should validate an array', function(done) {
+		it ('should validate an array', function(done) {
 			var objArr = {
 				key: ['string', {}]
 			};
@@ -107,7 +107,7 @@ describe('verja API', function() {
 		});
 
 		//need to fix the lib so this works
-		it('should validate a primative', function(done) {
+		it ('should validate a primative', function(done) {
 			verja.validate('some string', new verja.Field({type: 'string'}), function(e) {
 				if (e) { throw e; }
 				done();
@@ -115,14 +115,14 @@ describe('verja API', function() {
 		});
 
 		//need to fix the lib so this works
-		it('should validate simple nested objects and return null for errors in the callback', function(done) {
+		it ('should validate simple nested objects and return null for errors in the callback', function(done) {
 			verja.validate(goodNest, nestSchema, function(e) {
 				if (e !== null) { throw e; }
 				done();
 			});
 		});
 
-		it('should validate simple nested objects (invalid example)', function() {
+		it ('should validate simple nested objects (invalid example)', function() {
 			var errObj;
 			verja.validate(badNest, nestSchema, function(e) {
 				errObj = e;
