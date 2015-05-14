ee97af8b7c11f4b7441adebfef887930b35540d1
Forrest Almasi <forrest.almasi@gmail.com>
2015-05-14 14:05:35 -0400

    styling

--- tests/spec/validators/url.js
+++ tests/spec/validators/url.js
@@ -3,27 +3,27 @@ if (typeof require !== 'undefined') {
 }
 describe ('url validation rule', function() {
 	it ('should tell if a given string is a url', function(done) {
-		verja.validators.url('http://www.businessinsider.com', true, function(valid){
+		verja.validators.url('http://www.businessinsider.com', true, function(valid) {
 			if (valid) { return done(); }
 			throw new Error('valid url failed');
 		});
 	});
 	it ('should tell if a given string is a url', function(done) {
-		verja.validators.url('globsting', true, function(valid){
+		verja.validators.url('globsting', true, function(valid) {
 			if (!valid) { return done(); }
 			throw new Error('invalid url failed');
 		});
 	});
 
 	it ('should not throw a false positive for a url of all spaces', function(done) {
-		verja.validators.url('          ', true, function(valid){
+		verja.validators.url('          ', true, function(valid) {
 			if (!valid) { return done(); }
 			throw new Error('blank url url failed');
 		});
 	});
 
 	it ('should report an error if an input given is not a string', function(done) {
-		verja.validators.url(4, true, function(valid){
+		verja.validators.url(4, true, function(valid) {
 			if (!valid) { return done(); }
 			throw new Error('non-string failed');
 		});
